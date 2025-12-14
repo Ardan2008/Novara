158caae9a6b31a4ddf577a29dda61fe30a35dadf
Ardan <ramadhanardan69@gmail.com>
2025-12-15 00:07:23 +0700

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
-const navLogo = document.querySelector('#navbar_logo')
 const body = document.querySelector('body')
+const navLinks = document.querySelectorAll('.navbar_link')
 
-// Display Mobile Menu
+// Toggle hamburger
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
     body.classList.toggle('active')
 }
 
-menu.addEventListener('click', mobileMenu);
+menu.addEventListener('click', mobileMenu)
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (menu.classList.contains('is-active')) {
+            menu.classList.remove('is-active')
+            menuLinks.classList.remove('active')
+            body.classList.remove('active')
+        }
+    })
+})
